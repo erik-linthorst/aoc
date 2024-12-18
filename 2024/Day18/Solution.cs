f1f3cd43846f16e72010cf47c337ec951254a35f
Erik Linthorst <erik@lintsoft.nl>
2024-12-18 06:06:51 +0100

    2024/d18 puzzle

---
+++ 2024/Day18/Solution.cs
@@ -0,0 +1,21 @@
+namespace AdventOfCode.Y2024.Day18;
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Text;
+using System.Numerics;
+
+[ProblemName("RAM Run")]
+class Solution : Solver {
+
+    public object PartOne(string input) {
+        return 0;
+    }
+
+    public object PartTwo(string input) {
+        return 0;
+    }
+}
