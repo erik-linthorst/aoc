8d8cfa17fa5d1252bc32e22027af7895f2fe2beb
Erik Linthorst <erik@lintsoft.nl>
2024-12-22 07:41:33 +0100

    2024-d22 part 1

--- 2024/Day22/Solution.cs
+++ 2024/Day22/Solution.cs
@@ -1,6 +1,7 @@
 // ReSharper disable MemberCanBePrivate.Global
 
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventOfCode.Y2024.Day22;
 
@@ -9,7 +10,9 @@ using System;
 [ProblemName("Monkey Market")]
 class Solution : Solver {
     public object PartOne(string input) {
-        return 0;
+        
+        var secrets = input.Split(Environment.NewLine).Select(int.Parse).ToArray();
+        return secrets.Select(i => SecretNumbers(i, 2000).Last()).Sum();
     }
 
     public object PartTwo(string input) {
@@ -23,17 +26,16 @@ class Solution : Solver {
         }
     }
 
-    public long NextSecretNumber(long secret)
-    {
-        //# Step 1: Multiply by 64, mix, prune
+    public long NextSecretNumber(long secret) {
+        // Step 1: Multiply by 64, mix, prune
         secret ^= (secret * 64) % 16777216;
         secret %= 16777216;
 
-        //# Step 2: Divide by 32, round down, mix, prune
+        // Step 2: Divide by 32, round down, mix, prune
         secret ^= (secret / 32) % 16777216;
         secret %= 16777216;
 
-        //# Step 3: Multiply by 2048, mix, prune
+        // Step 3: Multiply by 2048, mix, prune
         secret ^= (secret * 2048) % 16777216;
         secret %= 16777216;
 
